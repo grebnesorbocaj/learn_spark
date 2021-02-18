36aeb5cd8a7bd431236601b04fc0754aa2364c0d
grebnesorbocaj <bocajgrebnesor@gmail.com>
2021-02-18 12:13:31 -0700

    fix: change directory name to reference dataset

---
+++ learn_spark/movies/ratings-counter.py
@@ -0,0 +1,18 @@
+from pyspark import SparkConf, SparkContext
+from learn_spark.config import BaseConfig
+from learn_spark.utils.runner import run_local
+import collections
+
+
+def ratings_counter(sc: SparkContext):
+    lines = sc.textFile(f"{BaseConfig.DATA_FOLDER}/{BaseConfig.MOVIE_LENS_FOLDERS}/u.data")
+    ratings = lines.map(lambda x: x.split()[2])
+    result = ratings.countByValue()
+
+    sortedResults = collections.OrderedDict(sorted(result.items()))
+    for key, value in sortedResults.items():
+        print("%s %i" % (key, value))
+
+if __name__ == "__main__":
+    sc = run_local("RatingsHistogram")
+    ratings_counter(sc)

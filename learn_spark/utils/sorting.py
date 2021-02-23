b49751006cf43d4ead4804907c592a0faa8f11f4
grebnesorbocaj <bocajgrebnesor@gmail.com>
2021-02-22 21:28:01 -0700

    dev: improve utils/sorting.py
    - add option for sortedResults
      - by Key or by Val (integer)
    
    - add cleanString to use regex to break up words

--- learn_spark/utils/sorting.py
+++ learn_spark/utils/sorting.py
@@ -1,6 +1,20 @@
 import collections
+import re
 
 
-def sortedResults(rdd):
-    results = rdd.collect()
-    return collections.OrderedDict(sorted(results))
+def sortedResults(rdd, by="key"):
+    if by == "key":
+        results = rdd.collect()
+        return collections.OrderedDict(sorted(results))
+    else if by == "intVal": 
+        results = rdd.collect()
+        valueKeySort = collections.OrderedDict(sorted(results, key=lambda x: int(x[1])))
+        return valueKeySort
+
+def cleanString(text: str):
+    """
+    Breaks up text based on words ('r/W+') with a unicode encoding
+    :param text: string input variable
+    :return:
+    """
+    return re.compile(pattern=r'/W+', flags=re.UNICODE).split(text.lower())
